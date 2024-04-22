fa7fb9c7a63d5f319f17801b439c3291b99c7362
cutekibry <cutekibry@gmail.com>
2024-04-22 17:04:32 +0800

    feat(footer): Change font size and add 🦦

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
 function Footer() {
   return (
     <div className="footer">
-      <p>&copy; Tsukimaru 2024</p>
+      <p>
+        &copy; Tsukimaru <FontAwesomeIcon icon={fas.faOtter} /> 2024
+      </p>
+      <p>
+        Inspired by <a href="https://todomvc.com/">TodoMVC</a>
+      </p>
+      <p>
+        Powered by <a href="https://vitejs.dev/">Vite</a> +{" "}
+        <a href="https://react.dev/">React</a>
+      </p>
     </div>
-	)
+  );
 }
-
 
 export default Footer;
